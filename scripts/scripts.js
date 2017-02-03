afd17a34f8f29dd3c4359ff5d83d6448337567ba
David Morison <davemorison@gmail.com>
2017-02-03 00:39:04 +0000

    adding in the cards

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+console.log(x + ' by ' + y);
+
+var header = angular.element(document).find('header');
+header.css('height', y + 'px');
