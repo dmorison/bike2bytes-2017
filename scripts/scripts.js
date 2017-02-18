53c7385df991a0e67a4f7a9b9d344780f706245f
David Morison <davemorison@gmail.com>
2017-02-18 18:19:26 +0000

    second phase styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-console.log(x + ' by ' + y);
+// var w = window,
+//     d = document,
+//     e = d.documentElement,
+//     g = d.getElementsByTagName('body')[0],
+//     x = w.innerWidth || e.clientWidth || g.clientWidth,
+//     y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+// console.log(x + ' by ' + y);
 
-var header = angular.element(document).find('header');
-header.css('height', y + 'px');
+// var header = angular.element(document).find('header');
+// header.css('height', y + 'px');
