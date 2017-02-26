a944107b9e918657f5ac2ee6691df249454fd701
David Morison <davemorison@gmail.com>
2017-02-26 16:43:12 +0000

    adding shiny app

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,9 @@ var app = angular.module('bikeBytesApp', []);
 
 app.factory('dataFeed', ['$http', function($http) {
 	function getFeed() {
-		return $http.get('https://docs.google.com/spreadsheets/d/1a4LB1LTsIxV5vGZDo2-mIHak6bnTbeiR6AQE1VQx0-M/pub?gid=0&single=true&output=csv');
+		// return $http.get('https://docs.google.com/spreadsheets/d/1a4LB1LTsIxV5vGZDo2-mIHak6bnTbeiR6AQE1VQx0-M/pub?gid=0&single=true&output=csv');
+		// return $http.get('https://spreadsheets.google.com/feeds/list/1a4LB1LTsIxV5vGZDo2-mIHak6bnTbeiR6AQE1VQx0-M/1/public/basic?alt=json');
+		return $http.get('https://spreadsheets.google.com/feeds/list/1a4LB1LTsIxV5vGZDo2-mIHak6bnTbeiR6AQE1VQx0-M/od6/public/basic?hl=en_US&alt=json');
 	};
 
 	return {
@@ -25,14 +27,26 @@ app.controller('bikeBytes', ['$scope', '$http', 'dataFeed', function($scope, $ht
 
 	dataFeed.getFeed.then(
 		function success(response) {
-			// console.log(response.data);
-			var feed = response.data;
-			var items = feed.split(',');
-			// console.log(items);
-			
-			$scope.pushups = items[3];
-			$scope.book = items[5];
-			$scope.parkrunpb = items[7];
+			// console.log(response.data.feed.entry[0].content.$t);
+			// console.log(response.data.feed.entry[1].content.$t);
+			// console.log(response.data.feed.entry[2].content.$t);
+			// console.log(response.data.feed.entry.length);
+
+			var pushups = response.data.feed.entry[0].content.$t.split(': ');
+			$scope.pushups = pushups[1];
+			var book = response.data.feed.entry[1].content.$t.split(': ');
+			$scope.book = book[1];
+			var parkrunpb = response.data.feed.entry[2].content.$t.split(': ');
+			$scope.parkrunpb = parkrunpb[1];
+
+			// var items = [];
+			// for (var i = 0; i <= response.data.feed.entry.length; i++) {
+			// 	var item = response.data.feed.entry[i].content.$t;
+			// 	item = item.split(': ');
+			// 	console.log(item[1]);
+			// 	items.push(item[1]);
+			// }
+			// console.log('my array: ' + items);
 		},
 		function error(response) {
 			console.log(response);
